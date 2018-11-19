bd29874b0d388bb75f8c1d5d0a182e7977c78373
FernandoBerrospi <luisfernando2033@gmail.com>
2018-11-19 13:38:32 -0500

    Update InicioController.php

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -14,7 +14,7 @@ class InicioController extends Controller
      */
     public function index()
     {
-        return view('inicio.index');
+        return view('inicio.Index');
     }
         public function nosotros()
     {
